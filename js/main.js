cc5a79a0300869d4f258a1aee00c318625e6111f
semastep96 <semastep5@gmail.com>
2021-12-04 15:27:32 +0300

    add logic

---
+++ js/main.js
@@ -0,0 +1,50 @@
+import {inputHigh, inputLow} from './view.js'
+
+function addInputListeners () {
+    function addListeners(input) {
+        function addTask() {
+            const taskContainer = input.parentElement.parentElement.querySelector('.task-container')
+            const text = input.value
+
+            if (!text) return
+
+            input.value = ''
+
+            let div = document.createElement('div')
+            div.classList.add('form-element')
+            div.innerHTML = `<label class="form-element_label">
+            <input type="checkbox" class="checkbox">
+            <span class="checkbox-text">${text}</span>
+            </label>
+            <button class="btn btn-remove"><img src="img/remove-icon.png" alt="remove icon" class="icon icon-remove"></button>`
+            taskContainer.insertAdjacentElement('afterbegin', div)
+
+            const btn = div.querySelector('.btn-remove')
+            btn.addEventListener('click', function () {
+                div.remove()
+            })
+
+            const label = div.querySelector('.form-element_label')
+            label.querySelector('input').addEventListener('click', function (ev) {
+                ev.stopPropagation()
+            })
+            label.addEventListener('click', function () {
+                div.classList.toggle('form-element--checked')
+                if (div.classList.contains('form-element--checked')) {
+                    taskContainer.insertAdjacentElement('beforeend', div)
+                } else {
+                    taskContainer.insertAdjacentElement('afterbegin', div)
+                }
+            })
+
+        }
+        input.addEventListener('onsubmit', addTask)
+        input.parentElement.querySelector('.btn-add').addEventListener('click', addTask)
+    }
+
+    addListeners(inputLow)
+    addListeners(inputHigh)
+}
+addInputListeners()
+
+
---
+++ js/view.js
@@ -0,0 +1,2 @@
+export const inputHigh = document.querySelector('#high')
+export const inputLow = document.querySelector('#low')
